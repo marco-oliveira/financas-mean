c3e24858407c856070670d2f6637221c6f813622
marco-oliveira <marco-oliveira@live.com>
2018-05-15 14:08:04 -0300

    Estrutura inicial do build do frontend com gulp

---
+++ frontend/gulpTasks/app.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp')
+
+gulp.task('app',['app.html', 'app.js', 'app.css', 'app.assets'])
+
+gulp.task('app.html', () => {})
+
+gulp.task('app.js', ()=>{})
+
+gulp.task('app.css', ()=>{})
+
+gulp.task('app.assets', ()=>{})
---
+++ frontend/gulpTasks/server.js
@@ -0,0 +1,5 @@
+const gulp = require('gulp')
+
+gulp.task('watch', ()=>{})
+
+gulp.task('server', ['watch'], () => {})
