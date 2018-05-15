d1d0effeba9f381bd92d209b307eef5734734a21
marco-oliveira <marco-oliveira@live.com>
2018-05-15 16:35:14 -0300

    Configurando rotas utilizando o Ui Router

---
+++ frontend/app/config/routes.js
@@ -0,0 +1,12 @@
+angular.module('financas').config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('dashboard', {
+        url: '/dashboard',
+        templateUrl: 'dashboard/dashboard.html'
+    })
+        .state('billingCycle', {
+            url: '/billingCycle',
+            templateUrl: 'billingCycle/tabs.html'
+        })
+
+    $urlRouterProvider.otherwise('/dashboard')
+}])
