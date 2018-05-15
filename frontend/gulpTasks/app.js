70d07d29c6eea7e23971364c97f986196192f1e3
marco-oliveira <marco-oliveira@live.com>
2018-05-15 14:58:21 -0300

    Infra-Estrutuda do build com gulp criada

--- frontend/gulpTasks/app.js
+++ frontend/gulpTasks/app.js
@@ -1,11 +1,34 @@
 const gulp = require('gulp')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
+const htmlmin = require('gulp-htmlmin')
 
 gulp.task('app',['app.html', 'app.js', 'app.css', 'app.assets'])
 
-gulp.task('app.html', () => {})
+gulp.task('app.html', () => {
+    return gulp.src('app/**/*.html')
+        .pipe(htmlmin({ conllapseWhitespace: true }))
+        .pipe(gulp.dest('public'))
+})
 
-gulp.task('app.js', ()=>{})
+gulp.task('app.js', ()=>{
+    return gulp.src('app/**/*.js')
+        .pipe(babel({presets: ['env']}))
+        .pipe(uglify())
+        .pipe(concat('app.min.js'))
+        .pipe(gulp.dest('public/assets/js'))
+})
 
-gulp.task('app.css', ()=>{})
+gulp.task('app.css', ()=>{
+    return gulp.src('app/**/*.css')
+        .pipe(uglifycss({ "uglyComments": true}))
+        .pipe(concat('app.min.css'))
+        .pipe(gulp.dest('public/assets/css'))
+})
 
-gulp.task('app.assets', ()=>{})
+gulp.task('app.assets', ()=>{
+    return gulp.src('assets/**/*.*')
+        .pipe(gulp.dest('public/assets'))
+})
