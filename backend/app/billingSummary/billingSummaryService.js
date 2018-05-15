2405222de21960852bd0b8e04ee6914549754f06
marco-oliveira <marco-oliveira@live.com>
2018-05-15 09:10:10 -0300

    Utilizando a api aggregate do mongoose no serviço para retornar o sumario de pagamentos

---
+++ backend/app/billingSummary/billingSummaryService.js
@@ -0,0 +1,21 @@
+const BillingCycle = require('../billingCycle/billingCycle')
+const _  = require('lodash')
+
+//Função middleware
+function getSummary(req, res, next) {
+    BillingCycle.aggregate({
+        $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}}
+    }, {
+        $group:{_id:null, credit:{$sum:"$credit"}, debt:{$sum:"$debt"}}
+    },{
+        $project:{_id:0, credit:1, debt:1}
+    }, function (error, result) {
+        if (error){
+            res.status(500).json({errors:[error]})
+        }else {
+            res.json(_.defaults(result[0], {credit: 0, debt: 0}))
+        }
+    })
+}
+
+module.exports = { getSummary }
