3e418b2c6fb9c999faa5541268c39c89915ffb8e
marco-oliveira <marco-oliveira@live.com>
2018-05-17 15:19:13 -0300

    Criando o component content-header

---
+++ frontend/app/common/components/contentHeader.js
@@ -0,0 +1,12 @@
+angular.module('financas').component('contentHeader', {
+    bindings: {
+        name: '@',
+        small: '@'
+    },
+    template: `
+        <section class="content-header">
+            <h1>{{ $ctrl.name }} <small>{{ $ctrl.small }}</small></h1>
+        </section>
+    
+    `
+})
