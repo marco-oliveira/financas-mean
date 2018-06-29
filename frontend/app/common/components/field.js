7e9d2cd070dd77fa358f1ba9db92a2d1a49806a6
marco-oliveira <marco-oliveira@live.com>
2018-06-29 09:08:42 -0300

    criando componente field

---
+++ frontend/app/common/components/field.js
@@ -0,0 +1,27 @@
+(function () {
+    'use strict'
+    angular.module('financas').component('field', {
+        bindings: {
+            id: '@',
+            label: '@',
+            placeholder: '@',
+            type: '@',
+            grid: '@',
+            model: '='
+
+        },
+        controller: ['gridSystem', function (gridSystem) {
+            this.$onInit = () => this.gridClass = gridSystem.toCssClass(this.grid)
+        }],
+        template: `
+         <div class="{{ $ctrl.gridClass }}">
+            <div class="form-group">
+                <label for="{{ $ctrl.id }}">{{ $ctrl.label }}</label>
+                <input id="{{ $ctrl.id }}" type="{{ $ctrl.type }}" class="form-control" 
+                    placeholder="{{ $ctrl.placeholder }}" ng-model="$ctrl.model">
+            </div>
+         </div>
+        
+        `
+    })
+})()
