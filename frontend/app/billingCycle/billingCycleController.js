5b2fb4ee11d716abbbc5e515835bcd3616edbe50
marco-oliveira <marco-oliveira@live.com>
2018-06-29 11:34:30 -0300

    implementando lista de pagamentos

--- frontend/app/billingCycle/billingCycleController.js
+++ frontend/app/billingCycle/billingCycleController.js
@@ -7,14 +7,24 @@
 
     function BillingCycleCtrl($http, messages) {
         const vm = this
-        vm.create = function () {
-            const url = 'http://localhost:3003/api/billingCycles'
-            $http.post(url, vm.billingCycle).then(function (response) {
+        const url = 'http://localhost:3003/api/billingCycles'
+
+        vm.reflesh = function(){
+            $http.get(url).then(function (response) {
                 vm.billingCycle = {}
+                vm.billingCycles = response.data
+            })
+        }
+
+        vm.create = function () {
+            $http.post(url, vm.billingCycle).then(function (response) {
+                vm.reflesh()
                 messages.addSuccess('Operação Realizada com Sucesso.')
             }).catch(function (response) {
                 messages.addError(response.data.errors)
             })
         }
+
+        vm.reflesh()
     }
 })()
